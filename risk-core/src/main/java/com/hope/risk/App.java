7cacf0c41029a799c44f88e416da82a1290ab05a
liuyongqiang826 <1491368215@qq.com>
2020-11-27 17:16:55 +0800

    risk

---
+++ risk-core/src/main/java/com/hope/risk/App.java
@@ -0,0 +1,13 @@
+package com.hope.risk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

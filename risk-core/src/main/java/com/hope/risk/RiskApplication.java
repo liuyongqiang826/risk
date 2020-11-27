c844c13ade16b208cc912e41706d252c7ca283a3
liuyongqiang826 <1491368215@qq.com>
2020-11-27 18:17:55 +0800

    update

--- risk-core/src/main/java/com/hope/risk/RiskApplication.java
+++ risk-core/src/main/java/com/hope/risk/RiskApplication.java
@@ -2,9 +2,11 @@ package com.hope.risk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableFeignClients("com.hope")
 public class RiskApplication {
 
 	public static void main(String[] args) {
